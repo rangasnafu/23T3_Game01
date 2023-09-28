93fab5201236ae1f91387d79bab85e9cd7487f2f
rangasnafu - Home <135566054+rangasnafu@users.noreply.github.com>
2023-09-28 23:12:44 +1000

    updated timer, game over (slime collision)

--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Slime : MonoBehaviour
 {
     public float moveSpeed = 1.0f;
 
+        public GameObject gameOverUI;
+
+
     private void Update()
     {
         Vector3 moveDirection = Vector3.up;
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+
+        if (gameOverUI.activeSelf && Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1f;
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene("Level1");
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+       
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Time.timeScale = 0f;
+            gameOverUI.SetActive(true);
+        }
     }
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,35 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
-    public TextMeshProUGUI timerText;
-    private float countdownTime = 60.0f; // 1 minute in seconds
-    private bool isCounting = true;
+    float currentTime = 0f;
+    float startingTime = 60f;
 
-    private void Update()
+    public GameObject gameOverTimeUI;
+    public GameObject gameOverUI;
+
+    public string newGameScene;
+
+    [SerializeField] TMP_Text countdownText;
+
+    public AudioSource audioSource;
+    public AudioClip timer;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        if (isCounting)
+        currentTime = startingTime;
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <=10)
         {
-            countdownTime -= Time.deltaTime;
-
-            if (countdownTime <= 0.0f)
-            {
-                countdownTime = 0.0f;
-                isCounting = false;
-            }
-
-            UpdateTimerText();
+            countdownText.color = Color.red;
         }
+
+        if (currentTime < 0)
+        {
+            Debug.Log("GameOver!");
+            Time.timeScale = 0f;
+            gameOverTimeUI.SetActive(true);
+
+        }
+
+        if (gameOverTimeUI.activeSelf && Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1f;
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene("Level1");
+        }
+      
+
     }
 
-    private void UpdateTimerText()
+    public void TimerSound()
     {
-        int minutes = Mathf.FloorToInt(countdownTime / 60);
-        int seconds = Mathf.FloorToInt(countdownTime % 60);
+        audioSource.clip = timer;
+        audioSource.Play();
 
-        string timerString = minutes.ToString() + ":" + (seconds < 10 ? "0" : "") + seconds.ToString();
-        timerText.text = timerString;
+        //if (Time.timeScale = 0f)
+        //{
+            //Destroy(gameObject);
+        //}
+        
     }
+
 }
