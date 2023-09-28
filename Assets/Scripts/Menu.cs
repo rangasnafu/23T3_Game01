67aff29520dbdecd79054ae2a2c6b5e4785a3e13
rangasnafu - Home <135566054+rangasnafu@users.noreply.github.com>
2023-09-29 02:47:51 +1000

    menu script but no menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
